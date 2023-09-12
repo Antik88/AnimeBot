7f9f054605ccd54a0372a3fa8a1e9c916acf0ea6
antik <ant1kow22@gamil.com>
2023-09-12 20:47:13 +0300

    add search anime

--- bot.py
+++ bot.py
@@ -1,29 +1,38 @@
+from pstats import Stats
 import time
 import logging
+import requests
+import urllib.parse
 import parse_anime as parser
-
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
-
 from aiogram.utils.callback_data import CallbackData
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-
-TOKEN = "6197474322:AAEWCPB5AG2uDOR6KjmP3xwONOg4xetIuvA"
+TOKEN = ""
 MSG = "Смотрел ли ты аниме сегодя, {}?"
 HELP_COMMAND = """
 /start - начать работу с ботом
 👍 anime - какое-то аниме
 🤓 help - список команд
+🔍 search - найти аниме по кадру
 """
 data = parser.read('data.json')
 
 kb = ReplyKeyboardMarkup(resize_keyboard=True)
 kb.add(KeyboardButton('🤓 help')).insert(KeyboardButton('👍 anime'))
+kb.add(KeyboardButton('🔍 search'))
 
+storage = MemoryStorage()
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot=bot)
+dp = Dispatcher(bot=bot, storage=storage)
 
 title_back = CallbackData("pref","title_id")
+
+class Form(StatesGroup):
+    waiting_for_photo = State()
 
 @dp.callback_query_handler(title_back.filter())
 async def button_press(call: types.CallbackQuery, callback_data: dict):
@@ -35,7 +44,6 @@ async def button_press(call: types.CallbackQuery, callback_data: dict):
                 media.attach_photo(title['screenshots'][i])
     await call.bot.send_media_group(call.message.chat.id, media=media)
 
-
 @dp.message_handler(commands=['start'])
 async def start_hendler(message: types.Message):
     user_id = message.from_user.id
@@ -43,7 +51,34 @@ async def start_hendler(message: types.Message):
     user_full_name = message.from_user.full_name
     logging.info(f'{user_id=} {user_full_name=} {time.asctime()}')
     await message.answer(f"Смотрел ли ты аниме сегодня, {user_name}?", reply_markup=kb)
-        
+
+@dp.message_handler(text='🔍 search')  
+async def cmd_start(message: types.Message):
+    await Form.waiting_for_photo.set()
+    await message.answer("Отправьте фото для поиска")
+
+@dp.message_handler(state=Form.waiting_for_photo, content_types=['photo'])
+async def process_photo(message: types.Message, state: FSMContext):
+    photo = message.photo[-1]
+    file = await bot.get_file(photo.file_id)
+    url = f"https://api.telegram.org/file/bot{TOKEN}/{file.file_path}"
+    response = requests.get(f"https://api.trace.moe/search?url={urllib.parse.quote_plus(url)}")
+
+    data = response.json()['result'][0]
+    video = data['video']
+    name = "Название аниме: " + data['filename'].split(']')[1].split('-')[0].strip()
+
+    result = types.MediaGroup()
+    result.attach_video(video, caption=name)
+
+    await bot.send_media_group(message.chat.id, result)
+
+    await state.finish()
+
+@dp.message_handler(state=Form.waiting_for_photo)
+async def process_not_photo(message: types.Message):
+    await message.reply("Жду фото")
+
 @dp.message_handler(text='🤓 help')
 async def help_command(message: types.Message):
     await message.answer(text=HELP_COMMAND)
@@ -73,7 +108,6 @@ async def send_img(message: types.Message):
                              callback_data=title_back.new(title_id=title.id))
     ilkb.add(line_btn1).add(get_imgs)
     await bot.send_photo(message.from_user.id, title.img, caption=caption, reply_markup=ilkb)
-
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
